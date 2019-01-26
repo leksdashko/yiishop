94f3b37e241dbaa34f457ea5d4916e10e7b68bf8
leksdashko <leksdashko@192.168.0.101>
2019-01-26 17:40:24 +0200

    добавил работу с бд

---
+++ controllers/CategoryController.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Andrey
+ * Date: 08.05.2016
+ * Time: 10:00
+ */
+
+namespace app\controllers;
+use app\models\Category;
+use app\models\Product;
+use Yii;
+
+class CategoryController extends AppController{
+
+    public function actionIndex(){
+        $hits = Product::find()->where(['hit' => '1'])->limit(6)->all();
+        return $this->render('index', compact('hits'));
+    }
+
+    public function actionView($id){
+        $id = Yii::$app->request->get('id');
+        $products = Product::find()->where(['category_id' => $id])->all();
+        return $this->render('view', compact('products'));
+    }
+
+} 
